86314ff2624954a8b6970ee70e030b8f4bfd9d25
muath-ye <34031333+muath-ye@users.noreply.github.com>
2025-07-11 23:00:14 +0300

    chore: update test_error.php

--- test_error.php
+++ test_error.php
@@ -6,4 +6,5 @@ error_reporting(E_ALL);
 echo "PHP Version: " . PHP_VERSION . "\n";
 echo "Error reporting level: " . error_reporting() . "\n";
 echo "Display errors: " . ini_get('display_errors') . "\n";
+echo "Current Version: v0.0.3 \n";
 ?>
